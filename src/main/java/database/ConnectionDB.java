cdb55436797ea5fc637b43af7c276d958171c8f5
musicsupermax <musicsupermax@gmail.com>
2018-11-16 15:00:13 +0200

    ORM creating a table in DB

--- src/main/java/database/ConnectionDB.java
+++ src/main/java/database/ConnectionDB.java
@@ -5,11 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionDB {
-    public static final String DRIVER = "com.mysql.jdbc.Driver";
-    public static final String DB = "catalog";
-    public static final String URL = "jdbc:mysql://localhost:3306/" + DB;
-    public static final String USER = "admin";
-    public static final String PASSWORD = "vcxz+3210";
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DB = "catalog";
+    private static final String URL = "jdbc:mysql://localhost:3306/" + DB;
+    private static final String USER = "admin";
+    private static final String PASSWORD = "vcxz+3210";
 
     public static Connection getConnection() throws SQLException {
         try {
